35c5dfa622b2965c2dcaad52b819ee1af0c52a58
Bored Certified <bored@example.com>
2025-05-09 12:21:53 -0600

    Style: Change edit icon to purple

--- app/components/SupabasePatientNotes.tsx
+++ app/components/SupabasePatientNotes.tsx
@@ -396,7 +396,7 @@ export default function SupabasePatientNotes({
                       }}
                       className="p-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
                     >
-                      <FiEdit />
+                      <FiEdit className="text-purple-600 dark:text-purple-500" />
                     </button>
                     <button
                       onClick={(e) => {
